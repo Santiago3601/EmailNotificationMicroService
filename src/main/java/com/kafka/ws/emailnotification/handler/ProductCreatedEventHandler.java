16ca127d491443c9fffd87ae523ee0e00d25b64e
Santiago <santyago3601@gmail.com>
2026-06-30 15:05:06 -0500

    Message class as a library, kafka listener working

--- src/main/java/com/kafka/ws/emailnotification/handler/ProductCreatedEventHandler.java
+++ src/main/java/com/kafka/ws/emailnotification/handler/ProductCreatedEventHandler.java
@@ -3,14 +3,17 @@ package com.kafka.ws.emailnotification.handler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import com.kafka.ws.core.ProductCreatedEvent;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
 public class ProductCreatedEventHandler {
-
+    
     @KafkaListener(topics = "product-created-event-topic")
-    public void handle() {
-
+    public void handle(ProductCreatedEvent event) {
+        log.info("Received a new event: {}", event.getTitle());
+        // Handle the event (e.g., send an email notification)
     }
 }
